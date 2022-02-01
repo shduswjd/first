c042f0e482f5c140a4fe6662ec6d5a4eab0082a8
shduswjd <98836333+shduswjd@users.noreply.github.com>
2022-02-02 01:59:30 +0800

    Delete 5_webscraping_basic directory

--- 5_webscraping_basic/5_user_agent.py
+++
@@ -1,9 +0,0 @@
-import requests
-from requests.sessions import HTTPAdapter
-url = "http://nadocoding.tistory.com"
-headers = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-
-with open("nadocoding.html", "w", encoding="utf8") as f:
-    f.write(res.text)
