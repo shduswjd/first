2b1e855cb1d9cdbb59e48856fd92972cbce0c94d
shduswjd <98836333+shduswjd@users.noreply.github.com>
2022-02-02 01:59:18 +0800

    Delete 4_모듈과 패키지 directory

--- 4_모듈과 패키지/퀴즈.py
+++
@@ -1,8 +0,0 @@
-# 모듈을 만들자.
-# 패키지는 항상 폴더 밖 그니까 그냥 python workspace의 하나의 파일에 저장해야해
-# 모듈과 패키지 같은 폴더에 모듈 저장 ㄴㄴ
-# import byme
-# print("이 프로그램은 나도코딩에 의해 만들어졌습니다.")
-# byme.sign("http://youtube.com")
-# 그래도 정답!!
-
